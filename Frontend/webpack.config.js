27603a4e99e5afa2c3ad9cfe8a7ff824c38165bc
luisparedes29 <luisparedescrv@gmail.com>
2023-03-17 00:19:12 -0400

    inclusion babel

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -24,8 +24,18 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)$/, // expresión regular que indica qué archivos procesar
                 type: 'asset/resource', // tipo de módulo a usar
                 generator: {
-                  filename: 'images/[name][ext]', // nombre del archivo generado
+                    filename: 'images/[name][ext]', // nombre del archivo generado
                 },
+            },
+            {
+                test: /\.js$/, // archivos que terminan en .js
+                exclude: /node_modules/, // excluir la carpeta node_modules
+                use: {
+                    loader: 'babel-loader', // usar el loader de babel
+                    options: {
+                        presets: ['@babel/preset-env'] // usar el preset de babel para convertir el código moderno
+                    }
+                }
             },
         ],
     },
